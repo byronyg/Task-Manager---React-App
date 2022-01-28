2a08c53d85ffbf67c3cafd97cbe17ed8fa940b45
Byron Yego <byronyego@gmail.com>
2022-01-28 12:31:24 +0300

    proptypes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello from React</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
-
 export default App;
