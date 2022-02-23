553f6f0149f115f5e94afe33f772533351dff0e2
Byron Yego <byronyego@gmail.com>
2022-02-23 14:31:53 +0300

    global state - props

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,5 @@
- import { useState } from "react"
+const Tasks = ( {tasks} ) => {
 
-const Tasks = () => {
-
-    const[tasks, setTasks] = useState([
-        {
-            id: 1, 
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 12.30pm',
-            reminder: true
-        },
-        {
-            id: 2, 
-            text: 'Meeting at School',
-            day: 'Feb 6th at 1.30pm',
-            reminder: true
-        },
-        {
-            id: 3, 
-            text: 'Shopping',
-            day: 'Feb 5th at 2.30pm',
-            reminder: false
-        },
-    ])
   return (
     <>
         {tasks.map((task) =>(<h3 key={task.id}>{task.text}</h3>))}
