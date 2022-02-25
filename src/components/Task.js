d1318b5101efba75f1589863bafa1e159dc0e24e
Byron Yego <byronyego@gmail.com>
2022-02-25 18:00:40 +0300

    task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,9 @@
 const Task = ({tasks}) => {
   return (
-    <div className="task"><h3>{tasks.text}</h3></div>
+    <div className="task">
+        <h3>{tasks.text}</h3>
+        <p>{tasks.day}</p>
+    </div>
   )
 }
 
